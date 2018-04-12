5d82908a061a78d3d0094eefc61bae02ef7e2b42
Nick <nickliqian@outlook.com>
2018-04-12 09:25:18 +0800

    abandon nianshao.me proxy site because this site alreay close

--- web_app.py
+++ web_app.py
@@ -83,6 +83,7 @@ if __name__ == '__main__':
     print(dir())
     for attr in dir():
         if attr.startswith("proxy__"):
+            # nianshao.me网站已经关闭，这里弃用
             if attr not in ["proxy__test", "proxy__nianshao"]:
                 # 所有proxy__开头的方法都加入jobs列表
                 jobs.append(threading.Thread(target=locals()[attr], args=(CONN_REDIS,)))
